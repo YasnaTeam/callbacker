8e649c90a0042bc6a96e94965642588446700e1e
Meysam GanJi <p.g.meysam@gmail.com>
2018-07-24 22:53:49 +0430

    add with requst method

--- logger/Initialize.go
+++ logger/Initialize.go
@@ -3,33 +3,35 @@ package logger
 import (
 	"github.com/sirupsen/logrus"
 	"os"
-	"github.com/t-tomalak/logrus-easy-formatter"
 	"net/http"
 )
 
-var log = &logrus.Logger {
-	Formatter: &easy.Formatter{
-		TimestampFormat: "2006-01-02 15:04:05.99999",
-		LogFormat:       "%lvl%[%time%] %msg%\n",
-	},
-}
+//var log *Logger
+//var log = &Logger{
+//	&logrus.Logger{
+//		//Formatter: &easy.Formatter{
+//		//	TimestampFormat: "2006-01-02 15:04:05.99999",
+//		//	LogFormat:       "%lvl%[%time%] %msg%\n",
+//		//},
+//	},
+//}
+var log = logrus.New()
 
 func New(debug bool) *logrus.Logger {
 	if debug {
 		log.SetLevel(logrus.DebugLevel)
 		log.SetOutput(os.Stdout)
+	} else {
+		log.SetLevel(logrus.FatalLevel)
 	}
 
 	return log
 }
 
-func WithRequest(r *http.Request) *logrus.Entry {
-	return log.WithFields(requestFields(r))
-}
-
-func requestFields(r *http.Request) logrus.Fields {
-	return logrus.Fields{
+func WithRequest(logger *logrus.Logger, r *http.Request) *logrus.Entry {
+	return logger.WithFields(logrus.Fields{
 		"method": r.Method,
-		"host": r.Host,
-	}
+		"protocol": r.Proto,
+		"remote_addr": r.RemoteAddr,
+	})
 }
