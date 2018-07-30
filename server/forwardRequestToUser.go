600086b76d00f76c2af1d5e81704e91c9748223a
Meysam GanJi <p.g.meysam@gmail.com>
2018-07-30 17:24:30 +0430

    forward request to user

--- server/forwardRequestToUser.go
+++ server/forwardRequestToUser.go
@@ -3,8 +3,19 @@ package server
 import (
 	"net"
 	"net/http"
+	"github.com/YasnaTeam/callbacker/common"
 )
 
 func forwardRequestToUser(conn net.Conn, r *http.Request) {
-	conn.Write([]byte("Hellooooooo"))
+	log.Debug("Send request to user connection...")
+	marshalledRequest, err := common.PrepareDataToSend(r)
+	if err == nil {
+		n, err := conn.Write(marshalledRequest)
+		if err != nil {
+			log.Error("There are some errors on forward callback to server", err)
+			return
+		}
+
+		log.Debugf("%d bytes of request, has been forwarded to the client", n)
+	}
 }
