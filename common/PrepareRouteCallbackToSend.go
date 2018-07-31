cb6c161b8c4ed3078a08cd29d533dd94f5a0a925
Meysam GanJi <p.g.meysam@gmail.com>
2018-07-31 23:48:50 +0430

    implement RouteCallback prepare function with two shorthand

--- common/PrepareRouteCallbackToSend.go
+++ common/PrepareRouteCallbackToSend.go
@@ -1,14 +1,5 @@
 package common
 
-import (
-	"vuvuzela.io/alpenhorn/log"
-)
-
 func PrepareRouteCallbackToSend(route, callback string) ([]byte, error) {
-	b, err := GetByteFromTransferable(&TransferableRouteCallback{"callback_forward", RouteCallback{route, callback}})
-	if err != nil {
-		log.Error(err)
-	}
-
-	return b, err
+	return PrepareRouteCallback(route, callback, "forward_callback")
 }
