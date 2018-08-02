156d5114e1ad1925c2764d19b2b18f10deb91686
Meysam GanJi <p.g.meysam@gmail.com>
2018-08-02 11:36:16 +0430

    implement callback url generator

--- common/GetInformationOfRequest.go
+++ common/GetInformationOfRequest.go
@@ -1,17 +1,27 @@
 package common
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
 
 func GetInformationOfRequest(r *http.Request, domain string) *Request {
 	var body = make([]byte, r.ContentLength)
 	r.Body.Read(body)
 	return &Request{
 		Method:   r.Method,
-		Callback: domain + "/" + r.URL.Path,
+		Callback: generateCallback(domain, r.URL.Path),
 		URL:      r.URL.String(),
 		Proto:    r.Proto,
 		Header:   Header(r.Header),
 		Body:     body,
 		Host:     r.Host,
 	}
+}
+
+func generateCallback(domain, callback string) string {
+	callback = strings.TrimLeft(callback, "/")
+	callbackInformation := strings.Split(callback, "/")
+
+	return domain + "/" + callbackInformation[1]
 }
